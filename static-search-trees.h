f831d0436934bbd81ffb14d657a96301323c7ecb
Samir Droubi <samir.t.droubi@gmail.com>
2021-03-29 16:39:48 -0400

    packed the static search trees into classes

---
+++ static-search-trees.h
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+const int NOT_FOUND_INDEX = -1;
+
+class CO_static_search_tree {
+    private:
+        int *tree;
+    public:
+    /*
+    Init a cache obilvious static search tree with size equal to
+    the next power of 2 greater than n
+    */
+    CO_static_search_tree(int n);
+    /*
+    Update the value at the given index
+    */
+    void update(int index, int value);
+
+    /*
+    Return the index of the successor of the value in the array.
+    If successor does not exists return NOT_FOUND_INDEX
+    */
+    int get(int value);
+}
