f21bc50534941889c01f7ba61486214df409122f
SamirDroubi <samir.t.droubi@gmail.com>
2021-04-12 16:39:34 -0400

    matrix_walker interface

---
+++ matrix_walker.h
@@ -0,0 +1,22 @@
+
+class matrix_walker {
+
+    public:
+        void teleport(int i, int j);
+        void move_left();
+        void move_right();
+        void move_up();
+        void move_down();
+        int get();
+};
+
+class co_matrix_walker {
+
+    public:
+        void teleport(int i, int j);
+        void move_left();
+        void move_right();
+        void move_up();
+        void move_down();
+        int get();
+};
